f15fcdb7ab9faf39055cada0314e043f1caef038
G-Man27 <gon.carvalho.oliveira@gmail.com>
2024-02-19 14:14:50 +0000

    ex5

--- Algoritmos e Programação/Fichas_Java/src/Ficha_01/Ex_05.java
+++ Algoritmos e Programação/Fichas_Java/src/Ficha_01/Ex_05.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class Ex_05 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in)
+        Scanner input = new Scanner(System.in);
         double n1, n2, n3, media_arit, media_pond;
+
+        System.out.print("Insira o 1º número: ");
+        n1= input.nextDouble();
+        System.out.print("Insira o 1º número: ");
+        n2= input.nextDouble();
+        System.out.print("Insira o 1º número: ");
+        n3= input.nextDouble();
+
         media_arit = (n1+n2+n3)/3;
 
+        media_pond= n1*0.2 + n2*0.3 + n3*0.5;
 
-
+        System.out.println("media_arit = " + media_arit);
+        System.out.println("media_pond = " + media_pond);
     }
 }
