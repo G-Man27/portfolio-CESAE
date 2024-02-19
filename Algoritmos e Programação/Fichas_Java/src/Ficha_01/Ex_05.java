67dc55ad85ca2d38925831033933d077eb87c72e
Gonçalo Oliveira <160496968+G-Man27@users.noreply.github.com>
2024-02-19 14:01:47 +0000

    Update Ex_05.java

--- Algoritmos e Programação/Fichas_Java/src/Ficha_01/Ex_05.java
+++ Algoritmos e Programação/Fichas_Java/src/Ficha_01/Ex_05.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Ex_05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in)
-        double n1, n2, n4, media_arit, media_pond;
+        double n1, n2, n3, media_arit, media_pond;
+        media_arit = (n1+n2+n3)/3;
 
 
 
