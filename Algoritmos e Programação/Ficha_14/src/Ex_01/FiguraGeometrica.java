301c7c38ce4e06ab26bc0bea8d97e0ab89c27538
G-Man27 <gon.carvalho.oliveira@gmail.com>
2024-04-18 10:10:46 +0100

    Ficha 14

---
+++ Algoritmos e Programação/Ficha_14/src/Ex_01/FiguraGeometrica.java
@@ -0,0 +1,19 @@
+package Ex_01;
+
+public abstract class FiguraGeometrica {
+    private String cor;
+
+    public FiguraGeometrica(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+    public void mostrarCor(){
+        System.out.println("A figura geométrica é: "+this.cor);
+    }
+
+    public abstract  double area();
+    public abstract  double perimetro();
+}
---
+++ Algoritmos e Programação/Ficha_14/src/Ex_01/Triangulo.java
@@ -0,0 +1,21 @@
+package Ex_01;
+
+public class Triangulo extends FiguraGeometrica {
+    private double base;
+    private double altura;
+    public Triangulo(String cor, double base, double altura) {
+        super(cor);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+        return (this.base*this.altura)/2;
+    }
+
+    @Override
+    public double perimetro() {
+        return this.base*3;
+    }
+}
