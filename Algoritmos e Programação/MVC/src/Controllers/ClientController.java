7011a597e8ca85a78af61bd03bd6bdda168456ac
G-Man27 <gon.carvalho.oliveira@gmail.com>
2024-05-08 17:02:24 +0100

    update

---
+++ Algoritmos e Programação/MVC/src/Controllers/ClientController.java
@@ -0,0 +1,31 @@
+package Controllers;
+
+import Domain.Product;
+import Domain.Sale;
+import Model.SalesRepository;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class ClientController {
+
+    public ClientController() {
+    }
+
+    public ArrayList<Product> getProductOfCategory(String productTypeToFind) throws FileNotFoundException {
+
+        System.out.println("Controller iniciado");
+        ArrayList<Sale> salesArray = SalesRepository.getInstance().getSalesArray();
+        ArrayList<Product> filteredProducts = new ArrayList<Product>();
+
+        for(Sale actualSale : salesArray){
+            if(actualSale.getProduct().getProductType().equals(productTypeToFind)){
+                filteredProducts.add(actualSale.getProduct());
+            }
+        }
+
+        return filteredProducts;
+
+    }
+
+}
---
+++ Algoritmos e Programação/MVC/src/Model/SalesRepository.java
@@ -0,0 +1,28 @@
+package Model;
+
+import Domain.Sale;
+import Tools.CSVSalesReader;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class SalesRepository {
+    private static SalesRepository instance;
+    private ArrayList<Sale> salesArray;
+
+    private SalesRepository() throws FileNotFoundException {
+        this.salesArray = CSVSalesReader.readCsvFileToArray("Files/minimercado.csv");
+    }
+
+    public static SalesRepository getInstance() throws FileNotFoundException {
+        if(instance==null){
+            instance= new SalesRepository();
+        }
+
+        return instance;
+    }
+
+    public ArrayList<Sale> getSalesArray() {
+        return salesArray;
+    }
+}
